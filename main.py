cde7ddf51d3c6f4ee14307ba44cb028a971c5db9
Sanford Mascarenhas <25857590+sanfordmascarenhas@users.noreply.github.com>
2023-03-27 00:28:55 -0400

    Complete program to train
    Added sequence creator.
    Added model creator and trainer.
    Added backup check.

--- main.py
+++ main.py
@@ -1,8 +1,32 @@
 import os
+import shutil
+import pickle
+import numpy as np
+from keras.utils import np_utils
+from keras.models import Sequential
+from keras.callbacks import ModelCheckpoint
+from keras.layers import LSTM, Dropout,Dense, BatchNormalization, Activation
 from music21 import converter, instrument, note, chord
 
-WEIGHTS_FILE_PATH = 'data/model_weights.hdf5'
-MIDI_FILE_PATH = 'midi_songs/'
+DATA_FOLDER_PATH = 'data/' # Dependant folder.
+WEIGHTS_FILE_PATH = DATA_FOLDER_PATH+'model_weights.hdf5' # Training checkpoint.
+NOTES_FILE_PATH = DATA_FOLDER_PATH+'notes' # This file is simply all the songs in one huge list dumped here.
+NETWORK_INPUT_FILE_PATH = DATA_FOLDER_PATH + 'network_input' # This contains a list of song sequences.
+NETWORK_OUTPUT_FILE_PATH = DATA_FOLDER_PATH + 'network_output' # This contains target notes for the network based on the sequences.
+MIDI_FILE_PATH = 'midi_songs/' # Put your midi tracks in this folder. 
+SEQUENCE_LENGTH = 100 # The model should take this length of notes/samples before trying to guess what should be next.
+EPOCH = 2
+BATCH_SIZE = 20 #128
+filepath = "weights-improvement-{epoch:02d}-{loss:.4f}-bigger.hdf5"
+checkpoint = ModelCheckpoint(
+        filepath,
+        monitor='loss',
+        verbose=1,
+        save_best_only=True,
+        mode='min'
+    )
+CALLBACKS_LIST = [checkpoint]
+
 
 def convert_midi_to_list(path_to_files):
     '''
@@ -45,20 +69,134 @@ def convert_midi_to_list(path_to_files):
             elif isinstance(element, note.Rest): # Find rest and append
                 notes.append('r')
                
-    # Implement save notes feature
+    # Dump the output into file. Creates a new one if it doesn't exist.
+    with open(NOTES_FILE_PATH, 'wb+') as filepath:
+        pickle.dump(notes, filepath)
 
     return notes
 
+def sequence_and_target(notes, size_of_vocabulary, sequence_length=100):
+    '''
+    Here, we will break the input list of notes into many lists of pre-defined lengths of sequences 
+    and separately, a note for each of them.
+
+    The idea is the model will use that sequence to try and guess the note. 
+    '''
+    # get a sorted vocabulary list
+    vocabulary = sorted(set(note for note in notes))
+
+    # Create a dictionary of note/number pairs for encoding.
+    note_to_number_dictionary = dict((note, number) for number, note in enumerate(vocabulary))
+
+    network_input = []
+    network_output = []
+
+    # Create sequences and corresponding target note.
+    for i in range(0, len(notes) - sequence_length, 1):
+        sequence = notes[i:i + sequence_length] # Slice list to make the sequence
+        target = notes[i + sequence_length] # Get the note right after that sequence.
+
+        network_input.append([note_to_number_dictionary[char] for char in sequence]) # This list will store the sequence after encoding it to numbers.
+        network_output.append(note_to_number_dictionary[target]) # This list will have the corresponding target notes after encoding.
+
+    number_of_sequences = len(network_input)
+
+    # reshape the input into a format compatible with LSTM layers
+    network_input = np.reshape(network_input, (number_of_sequences, sequence_length, 1))
+    # normalize input
+    # network_input = network_input / float(n_vocab)
+
+    # One Hot Encoding of sorts.
+    network_output = np_utils.to_categorical(network_output) 
+
+    # Dump the output into file. 
+    with open(NETWORK_INPUT_FILE_PATH, 'wb+') as filepath:
+        pickle.dump(network_input, filepath)
+    # Dump the output into file. 
+    with open(NETWORK_OUTPUT_FILE_PATH, 'wb+') as filepath:
+        pickle.dump(network_output, filepath)
+
+    # Give out the resultant network training data.
+    return (network_input,network_output)
+    
+def create_model(network_input,size_of_vocabulary):
+    '''
+    This function will create the model and return it, that's all.
+    '''
+    model = Sequential()
+    model.add(LSTM(
+        512,
+        input_shape=(network_input.shape[1], network_input.shape[2]),
+        recurrent_dropout=0.3,
+        return_sequences=True
+    ))
+    model.add(LSTM(512, return_sequences=True, recurrent_dropout=0.3,))
+    model.add(LSTM(512))
+    model.add(BatchNormalization())
+    model.add(Dropout(0.3))
+    model.add(Dense(256))
+    model.add(Activation('relu'))
+    model.add(BatchNormalization())
+    model.add(Dropout(0.3))
+    model.add(Dense(size_of_vocabulary))
+    model.add(Activation('softmax'))
+    model.compile(loss='categorical_crossentropy', optimizer='rmsprop')
+
+    return model
+
 
 # This is the main control flow.
-if __name__ == '__main__':
+if __name__ == '__main__':    
 
-    # First check if there's any checkpoint file.
-    if os.path.isfile(WEIGHTS_FILE_PATH):
-        print(f"{WEIGHTS_FILE_PATH} exists!\nWill train from here.")
+    # In case there's new midi files added, you might want to delete the data folder and calculate everything again.
+    if int(input("Do you want to re-initialize the data folder for new midi files? Press 1 else 0\n")):
+        print(f'Trying to delete the {DATA_FOLDER_PATH}...')
+        try:
+            shutil.rmtree(DATA_FOLDER_PATH)
+            print(f'{DATA_FOLDER_PATH} deleted successfully!')
+        except:
+            print(f'{DATA_FOLDER_PATH} did not exist.')
+
+
+    # This will create the data folder if it doesn't exist
+    if not os.path.exists(DATA_FOLDER_PATH):
+        print(f'folder {DATA_FOLDER_PATH} not found. Making one now ...')
+        os.makedirs(DATA_FOLDER_PATH) # Make data folder.
+
+    # Check for notes backup file
+    if not os.path.exists(NOTES_FILE_PATH): 
+        print(f'Notes backup file not found at {NOTES_FILE_PATH}. Parsing MIDI files now ...')
+        notes = convert_midi_to_list(MIDI_FILE_PATH) # Process midi files since backup not found.
     else:
-        print(f"{WEIGHTS_FILE_PATH} does not exist.\nWill create new checkpoint.")
+        with open(NOTES_FILE_PATH, 'rb') as filepath:
+            notes = pickle.load(filepath)
     
-    notes = convert_midi_to_list(MIDI_FILE_PATH)
-    print(notes)
+    # Check if there's any checkpoint file.
+    #if os.path.isfile(WEIGHTS_FILE_PATH):
+    #    print(f"{WEIGHTS_FILE_PATH} exists!\nWill train from here.")
+    #else:
+    #    print(f"{WEIGHTS_FILE_PATH} does not exist.\nWill create new checkpoint.")
+    
+    # Get the number of unique notes.
+    size_of_vocabulary = len(set(notes))
+
+    # Check for network_input backup file
+    if not os.path.exists(NETWORK_INPUT_FILE_PATH): 
+        print(f'Network input sequences backup file not found at {NETWORK_INPUT_FILE_PATH} and {NETWORK_OUTPUT_FILE_PATH}. Creating sequences now ...')
+        network_input, network_output = sequence_and_target(notes, size_of_vocabulary, SEQUENCE_LENGTH)
+    else:
+        with open(NETWORK_INPUT_FILE_PATH, 'rb') as filepath:
+            network_input = pickle.load(NETWORK_INPUT_FILE_PATH)
+        with open(NETWORK_OUTPUT_FILE_PATH, 'rb') as filepath:
+            network_output = pickle.load(NETWORK_OUTPUT_FILE_PATH)
+
+    
+    # This fellow does the hard work
+    model = create_model(network_input, size_of_vocabulary)
+
+    # Ask if this program should run in training mode or should it sing?
+    # TODO
+    
+
+    model.fit(network_input, network_output, epochs=EPOCH, batch_size=BATCH_SIZE, callbacks=CALLBACKS_LIST)
 
